614ca1a17426a4f2f7a39f0a956fd8e31c81364f
ammyt <a.m.tayea@gmail.com>
2017-01-16 19:39:16 +0100

    Update DoubleUp.java
    add findthematch

--- core/src/tuc/werkstatt/doubleup/DoubleUp.java
+++ core/src/tuc/werkstatt/doubleup/DoubleUp.java
@@ -47,7 +47,7 @@ public class DoubleUp extends Game {
     // add your individual minigame name (needs to match java file) here
     // index also being used as gameID in messages
     Array<String> minigames = new Array<String>(new String[]{
-            "CrazySmiley","PickColor", "PumpBalloon"  });
+            "CrazySmiley","PickColor", "PumpBalloon", "FindTheMatch"  });
 
     MiniGame currMiniGame = null;
     private String testingMiniGame = null;
