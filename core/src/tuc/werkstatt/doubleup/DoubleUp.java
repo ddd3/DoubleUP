45979e50d33e959678d16fb4a04211948abb204e
TUC-Project-DP <rotz.hausen@web.de>
2017-01-22 21:05:44 +0100

    New Game ClickTarget - Although not quite good enough! Wird noch verschönert

--- core/src/tuc/werkstatt/doubleup/DoubleUp.java
+++ core/src/tuc/werkstatt/doubleup/DoubleUp.java
@@ -48,7 +48,7 @@ public class DoubleUp extends Game {
     // add your individual minigame name (needs to match java file) here
     // index also being used as gameID in messages
     Array<String> minigames = new Array<String>(new String[]{
-            "CrazySmiley","PickColor", "PumpBalloon", "FindTheMatch","Drop","PlaneWarGame"  });
+            "ClickTarget", "CrazySmiley","PickColor", "PumpBalloon", "FindTheMatch","Drop","PlaneWarGame"  });
 
     MiniGame currMiniGame = null;
     private String testingMiniGame = null;
