9ff11deca690fe6ad6febd6a5a8193a5e6bb2c53
Daniel Wolff <dandev@posteo.de>
2017-02-04 21:39:52 +0100

    add spaceshooter to minigames, rebuild texture atlas

--- core/src/tuc/werkstatt/doubleup/DoubleUp.java
+++ core/src/tuc/werkstatt/doubleup/DoubleUp.java
@@ -66,7 +66,7 @@ public class DoubleUp extends Game {
     // add your individual minigame name (needs to match java file) here
     // index also being used as gameID in messages
     final String[] minigames = { "ShootingGallery", "CrazySmiley", "PickColor", "PumpBalloon",
-            "FindTheMatch", "Drop", "PlaneWarGame"};
+            "FindTheMatch", "Drop", "SpaceShooter"};
 
     MiniGame currMiniGame = null;
     private String testingMiniGame = null;
